a696104b89130eff350ea1400c4e8bbf0590ed9c
Hunter Freyer <yt@hjfreyer.com>
2025-09-01 15:34:44 +0000

    What if composing two functions is just "binding" to "result"?

--- exp2.py
+++ exp2.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, Callable
 import unittest
 
 
@@ -34,12 +34,10 @@ def str_iter_clone(state_tag: str, state_args: Any, msg: Any) -> Result:
     s, offset = msg
     return Result('result', (msg, s[offset]), 'end', None)
 
-def str_iter_equals(state_tag: str, state_args: Any, msg: Any) -> Result:
+
+def str_iter_equals_body(state_tag: str, state_args: Any, msg: Any) -> Result:
     if state_tag == 'start':
-        s, iter = msg
-        return Result('continue', (), 'loop_body', (s, 0, iter))
-    elif state_tag == 'loop_body':
-        s, offset, iter = state_args
+        s, offset, iter = msg
         return Result('iter_next', iter, 'iter_next_cb', (s, offset))
     elif state_tag == 'iter_next_cb':
         s, offset = state_args
@@ -47,9 +45,9 @@ def str_iter_equals(state_tag: str, state_args: Any, msg: Any) -> Result:
         str_has_next = offset < len(s)
 
         if not iter_has_next and not str_has_next:
-            return Result('result', True, 'end', None)
+            return Result('break', True, 'end', None)
         elif not iter_has_next or not str_has_next:
-            return Result('result', False, 'end', None)
+            return Result('break', False, 'end', None)
         else:
             return Result('iter_clone', iter, 'iter_clone_cb', (s, offset))
     elif state_tag == 'iter_clone_cb':
@@ -57,11 +55,71 @@ def str_iter_equals(state_tag: str, state_args: Any, msg: Any) -> Result:
         iter, iter_char = msg
         str_char = s[offset]
         if iter_char == str_char:
-            return Result('continue', (), 'loop_body', (s, offset + 1, iter))
+            return Result('next_loop', (s, offset + 1, iter), 'end', None)
         else:
-            return Result('result', False, 'end', None)
+            return Result('break', False, 'end', None)
     else:
         assert False, "Bad state: "+state_tag
+
+@dataclass
+class ForLoop:
+    body : Callable[[str, Any, Any], Result]
+
+    def __call__(self, state_tag: str, state_args: Any, msg: Any) -> Result:
+        def call_body(state_tag: str, state_args: Any, msg: Any) -> Result:
+            body_result = self.body(state_tag, state_args, msg)
+            if body_result.action == 'next_loop':
+                return Result('continue', body_result.action_args, 'body', ('start', ()))
+            elif body_result.action == 'break':
+                return Result('result', body_result.action_args, 'end', None)
+            else:
+                return Result(body_result.action, body_result.action_args, 'body', (body_result.resume_state_tag, body_result.resume_state_args))
+        
+        if state_tag == 'start':
+            return call_body('start', state_args, msg)  
+        elif state_tag == 'body':
+            inner_state_tag, inner_state_args = state_args
+            return call_body(inner_state_tag, inner_state_args, msg)
+        else:
+            assert False, "Bad state: "+state_tag
+
+@dataclass
+class Bound:
+    inner : Callable[[str, Any, Any], Result]
+    handlers : dict[str, Callable[[str, Any, Any], Result]]
+
+    def __call__(self, state_tag: str, state_args: Any, msg: Any) -> Result:
+        def call_handler(handler_name: str, msg: Any, handler_state_tag: str, handler_state_args: Any, inner_state_tag: str, inner_state_args: Any) -> Result:
+            handler = self.handlers[handler_name]
+            handler_result = handler(handler_state_tag, handler_state_args, msg)
+            if handler_result.action == 'resume':
+                return Result('continue', handler_result.action_args, 'inner', (inner_state_tag, inner_state_args))
+            else:
+                return Result(handler_result.action, handler_result.action_args, 'handler', (handler_name, handler_result.resume_state_tag, handler_result.resume_state_args, inner_state_tag, inner_state_args))
+        def call_inner(state_tag: str, state_args: Any, msg: Any) -> Result:
+            inner_result = self.inner(state_tag, state_args, msg)
+            return call_handler(inner_result.action, inner_result.action_args, 'start', (), inner_result.resume_state_tag, inner_result.resume_state_args)
+        if state_tag == 'start':
+            return call_inner('start', state_args, msg)
+        elif state_tag == 'inner':
+            inner_state_tag, inner_state_args = state_args
+            return call_inner(inner_state_tag, inner_state_args, msg)
+        elif state_tag == 'handler':
+            handler_name, handler_state_tag, handler_state_args, inner_state_tag, inner_state_args = state_args
+            return call_handler(handler_name, msg, handler_state_tag, handler_state_args, inner_state_tag, inner_state_args)
+        else:
+            assert False, "Bad state: "+state_tag
+
+
+
+def str_iter_equals_preamble(state_tag: str, state_args: Any, msg: Any) -> Result:
+    assert state_tag == 'start'
+    s, iter = msg
+    return Result('result', (s, 0, iter), 'end', None)
+
+str_iter_equals = Bound(str_iter_equals_preamble, {
+    'result': ForLoop(str_iter_equals_body),
+})
 
 
 def assertTranscript(test : unittest.TestCase, machine : Any, transcript : list[tuple[Any, str, Any]]):
