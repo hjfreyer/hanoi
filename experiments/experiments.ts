6fafaf9c096312506c17a9c208074cc622d19e02
Hunter Freyer <yt@hjfreyer.com>
2025-09-09 22:46:31 +0000

    Different kind of binding machine.

--- experiments/experiments.ts
+++ experiments/experiments.ts
@@ -1,4 +1,4 @@
-import { assert } from "console";
+import assert from "assert";
 
 export type Result<S> = {
   action: string;
@@ -29,5 +29,63 @@ export function transformer<S>(f: (msg: any) => any): Machine<["start"]> {
       msg: f(msg),
       resume_state: state,
     };
+  };
+}
+
+export type AndThenState<F, G> = ["start"] | ["first", F] | ["second", G];
+
+export type Startable<T> = T | ["start"];
+
+export function andThen<F, G>(f: Machine<Startable<F>>, g: Machine<Startable<G>>): Machine<AndThenState<Startable<F>, Startable<G>>> {
+  return (state: AndThenState<Startable<F>, Startable<G>>, msg: any): Result<AndThenState<Startable<F>, Startable<G>>> => {
+    if (state[0] === "start") {
+      return { action: "continue", msg: msg, resume_state: ["first", ["start"]] };
+    }
+    if (state[0] === "first") {
+      const result = f(state[1], msg);
+      if (result.action === "result") {
+        return { action: "continue", msg: result.msg, resume_state: ["second", ["start"]] };
+      }
+      return { action: result.action, msg: result.msg, resume_state: ["first", result.resume_state] };
+    }
+    if (state[0] === "second") {
+      const result = g(state[1], msg);
+      return { action: result.action, msg: result.msg, resume_state: ["second", result.resume_state] };
+    }
+    throw Error("Bad state: " + state[0]);
+  };
+}
+
+export type CallState<I, O, H> = ["start"] | ["inner", I, O] | ["handler", I, Startable<H>] | ["end"];
+
+export function call<I, O, H>(f: Machine<I>, handler: Machine<Startable<H>>): Machine<CallState<I, Startable<O>, Startable<H>>> {
+  return (state: CallState<I, Startable<O>, Startable<H>>, msg: any): Result<CallState<I, Startable<O>, Startable<H>>> => {
+    if (state[0] === "start") {
+      let [outer_state, inner_state, inner_msg] = msg;
+      return { action: "continue", msg: inner_msg, resume_state: ["inner", inner_state, outer_state] };
+    }
+    if (state[0] === "inner") {
+      let inner_state = state[1];
+      let outer_state = state[2];
+      const result = f(inner_state, msg);
+      if (result.action === "result") {
+        return { action: "result", msg: [outer_state, result.resume_state, result.msg], resume_state: ["end"] };
+      }
+      if (result.action === "continue") {
+        return { action: "continue", msg: result.msg, resume_state: ["inner", result.resume_state, outer_state] };
+      }
+      return { action: "continue", msg: [result.action, outer_state, result.msg], resume_state: ["handler", result.resume_state, ["start"]] };
+    }
+    if (state[0] === "handler") {
+      let inner_state = state[1];
+      let handler_state = state[2];
+      const handler_result = handler(handler_state, msg);
+      if (handler_result.action === "result") {
+        let [new_outer_state, new_msg] = handler_result.msg;
+        return { action: "continue", msg: new_msg, resume_state: ["inner", inner_state, new_outer_state] };
+      }
+      return { action: handler_result.action, msg: handler_result.msg, resume_state: ["handler", inner_state, handler_result.resume_state] };
+    }
+    throw Error("Bad state: " + state[0]);
   };
 }
--- experiments/experiments_test.ts
+++ experiments/experiments_test.ts
@@ -1,6 +1,6 @@
 // Test file for experiments.ts
 import assert from 'assert';
-import { Machine, Result, transformer } from './experiments';
+import { andThen, call, HandlerResult, Machine, Result, transformer } from './experiments';
 
 function handleContinue<S>(machine: Machine<S>, state: S, input: any): [S, string, any] {
   let result = machine(state, input);
@@ -43,7 +43,7 @@ const str_iter_init = transformer((s: string) => [s, -1]);
 
 type StrIterState = [string, number];
 
-function str_iter([str, offset]: StrIterState, msg: ['next' | 'clone']) {
+function str_iter([str, offset]: StrIterState, msg: ['next' | 'clone']): Result<StrIterState> {
   if (msg[0] === 'next') {
     offset += 1;
     return {
@@ -59,7 +59,7 @@ function str_iter([str, offset]: StrIterState, msg: ['next' | 'clone']) {
       resume_state: [str, offset],
     };
   }
-  assert(false, 'Bad msg: ' + msg[0]);
+  throw Error('Bad msg: ' + msg[0]);
 }
 
 describe('StrIter', () => {
@@ -88,7 +88,6 @@ type StringIterEqualsState = ["start"] | ["await_next", string] | ["await_clone"
 
 
 function string_iter_equals(state: StringIterEqualsState, msg: any): Result<StringIterEqualsState> {
-  console.log(`string_iter_equals: ${state} ${msg}`);
   if (state[0] === "start") {
     const s = msg;
     return {
@@ -140,7 +139,7 @@ function string_iter_equals(state: StringIterEqualsState, msg: any): Result<Stri
       };
     }
   }
-  assert(false, 'Bad state: ' + state[0]);
+  throw Error('Bad state: ' + state[0]);
 }
 
 describe('StringIterEquals', () => {
@@ -161,6 +160,85 @@ describe('StringIterEquals', () => {
       [true, 'iter', ['clone']],
       ['o', 'iter', ['next']],
       [false, 'result', true],
+    ]);
+  });
+
+  test('should work with iter shorter than string', () => {
+    assertTranscript(string_iter_equals, [
+      ['foo', 'iter', ['next']],
+      [true, 'iter', ['clone']],
+      ['f', 'iter', ['next']],
+      [true, 'iter', ['clone']],
+      ['o', 'iter', ['next']],
+      [false, 'result', false],
+    ]);
+  });
+
+  test('should work with string shorter than iter', () => {
+    assertTranscript(string_iter_equals, [
+      ['foo', 'iter', ['next']],
+      [true, 'iter', ['clone']],
+      ['f', 'iter', ['next']],
+      [true, 'iter', ['clone']],
+      ['o', 'iter', ['next']],
+      [true, 'iter', ['clone']],
+      ['o', 'iter', ['next']],
+      [true, 'result', false],
+    ]);
+  });
+
+  test('should work with char mismatch', () => {
+    assertTranscript(string_iter_equals, [
+      ['foo', 'iter', ['next']],
+      [true, 'iter', ['clone']],
+      ['f', 'iter', ['next']],
+      [true, 'iter', ['clone']],
+      ['r', 'result', false],
+    ]);
+  });
+});
+
+function passThroughHandler<S>(handler_name: string, state: S, msg: any): HandlerResult<S> {
+  return { kind: "continue", action: handler_name, msg: msg, handler_state: state };
+}
+
+function nullHandlerMachine(state: ["start"], msg: any): Result<["start"]> {
+  throw Error('Bad state: ' + state[0]);
+}
+
+type RandomHandlerState = ["start" | "end"] | ["iter", StrIterState];
+
+const string_iter_equals_inverse = andThen(andThen(andThen(andThen(
+  transformer((s: string) => [s, ["start"], s]),
+  call(str_iter_init, nullHandlerMachine)),
+  transformer(([s, constructor_state, iter_state]: [string, ["start"], StrIterState]) =>
+    [iter_state, ["start"], s])),
+  call(string_iter_equals, (s: RandomHandlerState, msg): Result<RandomHandlerState> => {
+    if (s[0] === "start") {
+      let [action, iter_state, iter_msg] = msg;
+      if (action !== "iter") {
+        throw Error('Bad action: ' + action);
+      }
+      return { action: "continue", msg: iter_msg, resume_state: ["iter", iter_state] };
+    }
+    if (s[0] === "iter") {
+      let iter_state = s[1];
+      const result = str_iter(iter_state, msg);
+      if (result.action === "result") {
+        return { action: "result", msg: [result.resume_state, result.msg], resume_state: ["end"] };
+      } else {
+        return { action: result.action, msg: result.msg, resume_state: ["iter", result.resume_state] };
+      }
+    }
+    throw Error('Bad state: ' + s[0]);
+  }
+  )), transformer(([iter_state, iter_equals_state, result]: [StrIterState, StringIterEqualsState, boolean]) => result));
+
+
+describe('StringIterEqualsInverse', () => {
+  test('should work', () => {
+    assertTranscript(string_iter_equals_inverse, [
+      ['foo', 'result', true],
     ]);
   });
 });
